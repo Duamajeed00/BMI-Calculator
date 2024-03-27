878fa184a0dcec38633c975e554b4c9780f677d0
DuaMajeed <160130460+Duamajeed00@users.noreply.github.com>
2024-03-28 03:04:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let weightInKg = 70;
+let heightInMeters = 1.75;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`your bmi is ${bmi}`);
+export {};
---
+++ index.ts
@@ -0,0 +1,4 @@
+let weightInKg:number = 70;
+let heightInMeters:number = 1.75;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`your bmi is ${bmi}`);
